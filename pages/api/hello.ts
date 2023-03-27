d0595730c712204dd0e6e02821a0209bae8f07b5
dawkaka <yousiph77@gmail.com>
2023-03-27 17:15:18 +0000

    feat: audio handler

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,41 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
+import FormData from 'form-data';
 
-type Data = {
-  name: string
+// Replace these with your actual OpenAI and Google Translate API keys
+const OPENAI_API_KEY = 'YOUR_OPENAI_API_KEY';
+const GOOGLE_TRANSLATE_API_KEY = 'YOUR_GOOGLE_TRANSLATE_API_KEY';
+
+async function transcribeAudio(audio: Blob): Promise<string> {
+  // Make an API call to OpenAI's Whisper API to transcribe the audio
+  const form = new FormData();
+  form.append('audio', audio);
+  const response = await axios.post('https://api.openai.com/v1/whisper/recognize', form, {
+    headers: {
+      Authorization: `Bearer ${OPENAI_API_KEY}`,
+      'Content-Type': `multipart/form-data; boundary=${form.getBoundary()}`,
+    },
+  });
+  return response.data.text;
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+async function translateText(text: string, targetLanguage: string): Promise<string> {
+  // Make an API call to Google Translate API to translate the text to the target language
+  const response = await axios.post(`https://translation.googleapis.com/language/translate/v2?key=${GOOGLE_TRANSLATE_API_KEY}`, {
+    q: text,
+    target: targetLanguage,
+  });
+  return response.data.data.translations[0].translatedText;
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { audio, targetLanguage } = req.body;
+
+  // Transcribe the audio using OpenAI's Whisper API
+  const text = await transcribeAudio(audio);
+
+  // Translate the transcribed text to the target language using Google Translate API
+  const translation = await translateText(text, targetLanguage);
+
+  res.status(200).json({ translation });
 }
